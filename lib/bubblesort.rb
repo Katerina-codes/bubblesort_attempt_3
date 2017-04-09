d692cf61cae25a0e82be5c72eacddfbe24b6b18d
katerina georgiou <katerinaldg@gmail.com>
2017-04-09 12:46:20 +0100

    Sorts a 2 element sorted list in a less specific way

--- lib/bubblesort.rb
+++ lib/bubblesort.rb
@@ -3,9 +3,7 @@ class Bubblesort
 def self.sort(list)
   if list == [] || list.length == 1
     list
-  elsif list == [1, 2]
-    list
-  elsif list == [1, 3]
+  elsif list[0] < list[1]
     list
   elsif list == [2, 1]
     [1, 2]
