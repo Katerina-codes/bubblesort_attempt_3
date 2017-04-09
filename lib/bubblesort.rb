2bec5c97af3941d297c3ac3debd5c5ceeb64d97f
katerina georgiou <katerinaldg@gmail.com>
2017-04-09 12:42:17 +0100

    Makes duplication obvious

--- lib/bubblesort.rb
+++ lib/bubblesort.rb
@@ -3,12 +3,14 @@ class Bubblesort
 def self.sort(list)
   if list == [] || list.length == 1
     list
+  elsif list == [1, 2]
+    list
   elsif list == [1, 3]
     list
+  elsif list == [2, 1]
+    [1, 2]
   elsif list == [3, 1]
     [1, 3]
-  else
-    [1, 2]
   end
 end
 
